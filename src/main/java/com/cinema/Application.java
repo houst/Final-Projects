58604d4a2b9d63e51f231d6a4e60a6b2d733c6f8
houst <o.litvinenko@hotmail.com>
2019-09-15 17:38:11 +0300

    Test commit

--- src/main/java/com/cinema/Application.java
+++ src/main/java/com/cinema/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) throws Exception {
-		// TODO: Remove this comment
-		System.out.println("Hello World");
 		SpringApplication.run(Application.class, args);
 	}
 }
